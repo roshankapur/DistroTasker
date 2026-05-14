98168afb10e22c8cf00875e7409a908f96228e72
Roshan Kapur <kapurroshan2003@gmail.com>
2026-05-14 23:15:32 +0530

    created project scaffolding

---
+++ src/main/java/com/distrotasker/distrotasker/DistrotaskerApplication.java
@@ -0,0 +1,13 @@
+package com.distrotasker.distrotasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrotaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrotaskerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/distrotasker/distrotasker/DistrotaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distrotasker.distrotasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistrotaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

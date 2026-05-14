1dcf45dfa6be4b679d494e7ad9995a4ea1569dea
Roshan Kapur <kapurroshan2003@gmail.com>
2026-05-15 02:08:19 +0530

    bogus

--- src/main/java/kapur/DistroTasker.java
+++ src/main/java/kapur/DistroTasker.java
@@ -1,13 +1,13 @@
-package com.distrotasker.distrotasker;
+package kapur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DistrotaskerApplication {
+public class DistroTasker {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DistrotaskerApplication.class, args);
+		SpringApplication.run(DistroTasker.class, args);
 	}
 
 }

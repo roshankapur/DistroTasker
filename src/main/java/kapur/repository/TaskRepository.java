dd000da17331f6d8e025f8025a3c0264a8a85c0f
Roshan Kapur <kapurroshan2003@gmail.com>
2026-05-16 14:45:23 +0530

    written prototype repo layer

---
+++ src/main/java/kapur/repository/TaskRepository.java
@@ -0,0 +1,29 @@
+package kapur.repository;
+
+import kapur.model.Task;
+import kapur.model.TaskStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+/** Task fields
+ * private UUID id;
+ * private String scriptPath;
+ * private LocalDateTime scheduledTime;
+ * private TaskStatus status;
+ */
+/** we need to be able to "trash" scripts after they're marked done
+ * through some form of marked autodelete when the backend crawls through the database.
+ * an entire database crawl could be computationally complex
+ * --> there should be a periodic DELETE WHERE trash=true call from frontend to keep backend clean*/
+public interface TaskRepository {
+    void save(Task task);
+    Task findByID(UUID id);
+    List<Task> findAll();
+    void deleteById(UUID id);
+    //add update functions
+    void updateScriptPath(UUID id, String newScriptPath);
+    void updateScheduledTime(UUID id, LocalDateTime newScheduledTime);
+    void updateTaskStatus(UUID id, TaskStatus newTaskStatus);
+}

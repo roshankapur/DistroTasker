be4bb0b5f4cdde3f14e94567d3df98b885dcbe64
Roshan Kapur <kapurroshan2003@gmail.com>
2026-05-29 13:05:48 +0530

    moving to postgres and JPA

--- src/main/java/kapur/model/Task.java
+++ src/main/java/kapur/model/Task.java
@@ -1,18 +1,31 @@
 package kapur.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-/** Lombok autowires getters, setters, toString and other utils with annotations*/
+/** Lombok autowires getters, setters, toString and other utils with annotations
+ *  JPA annotations map this class to the "tasks" table in PostgreSQL
+ *  Hibernate auto-generates DDL (CREATE TABLE, ALTER TABLE) from these annotations
+ *
+ *  JPA == jakarta persistence api
+ *  Entity tag specifies Task class as a database entity/table row
+ *  and obv Table tag is naming the table in postgres
+ *  */
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tasks")
 public class Task {
+    @Id //mark as primary key
     private UUID id;
     private String scriptPath;
     private LocalDateTime scheduledTime;
+    //jpa needs to know the datatype of enum before it wires in SQL
+    @Enumerated(EnumType.STRING)
     private TaskStatus status;
-    //might have to write run function through class Task implements Runnable to use in thread pool
 }

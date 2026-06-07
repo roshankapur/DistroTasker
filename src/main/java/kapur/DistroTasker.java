917c88186b6dfeceea77c1b63434f4c4cafbb42f
Roshan Kapur <kapurroshan2003@gmail.com>
2026-06-07 21:25:18 +0530

    enabled asynch on spring to allow more breathing room to main app thread

--- src/main/java/kapur/DistroTasker.java
+++ src/main/java/kapur/DistroTasker.java
@@ -2,9 +2,11 @@ package kapur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+//@EnableAsync makes Spring create a managed thread pool for async method calls
 @SpringBootApplication
+@EnableAsync
 //@EnableScheduling //commented out cause spring scheduling is not using ScheduledExecutorService and its threadpool
 public class DistroTasker {
 
